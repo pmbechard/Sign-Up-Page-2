24b061d02a21f8b38fb1b49fa15c64d228e17d6c
Peyton Bechard <pbechard@my.yorku.ca>
2022-05-29 19:07:29 +0800

    Selects elements for validation in js file and adds regex patterns

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 import './style.css';
+
+const form = document.getElementsByTagName('form');
+const name = document.getElementById('name');
+const email = document.getElementById('email');
+const postalCode = document.getElementById('postal-code');
+const country = document.getElementById('country');
+const pwd = document.getElementById('pwd');
+const confirmPwd = document.getElementById('confirm-pwd');
+const agree = document.getElementById('agree');
+
+const CAN_POSTAL_CODE = '/^[a-zA-z]\d[a-zA-z]\s?\d[a-zA-z]\d$/';
+const US_POSTAL_CODE = '/(\d{5}([\-]\d{4})?)/';
+
+form.addEventListener('submit', () => {});
